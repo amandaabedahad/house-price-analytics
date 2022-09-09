361eb1f0cf7f28766b5aaebef008d46f09e3e505
amandaabedahad <amanda.abedahad@outlook.com>
2022-09-09 13:36:24 +0200

    first draft of KNN for interpolation to substitue Nan values in columns

--- process_data.py
+++ process_data.py
@@ -5,6 +5,10 @@ from fake_useragent import UserAgent
 from geopy.geocoders import Nominatim
 import re
 from tqdm import tqdm
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
 
 ua = UserAgent()
 header = {
@@ -13,6 +17,7 @@ header = {
 
 
 def clean_address_sample(sample):
+    # TODO: double check if it parses ex Hejhej 3 A correctly (space between 3 and A)
     match = re.search("([A-Ö|a-ö]+.)*(\d+)*(\w)*", sample)
     str_match = match[0]
     return str_match
@@ -30,6 +35,37 @@ def get_long_lat(sample, city='Göteborgs kommun'):  # TODO: change this hardcod
     if location is None:
         return None, None
     return location.latitude, location.longitude
+
+
+def dummy_encoding(column_to_be_encoded):
+    dummy_enc = pd.get_dummies(column_to_be_encoded, prefix="Type")
+    return dummy_enc
+
+
+def interpolate_missing_data_KNN(dataframe, column):
+    # interpolates by taking the average of the nearby houses, implemented using KNN regression
+    # data_that_needs_interpolation =
+    dataframe.dropna(subset=[column, 'longitude', "nr_rooms"], inplace=True)
+
+    for housing_type in dataframe["housing_type"].unique():
+        rslt_data = dataframe[dataframe["housing_type"] == housing_type]
+
+        if housing_type == "Lägenhet":
+            X = rslt_data[["sqr_meter", "nr_rooms", "rent_month", "final_price", "latitude", "longitude"]]
+        elif housing_type == "Villa":
+            X = rslt_data[["sqr_meter", "nr_rooms", "final_price", "land_area", "other_srq", "latitude", "longitude"]]
+        else:
+            continue
+        param = {"n_neighbors": range(1, 20), "weights": ["uniform", "distance"]}
+        neigh = GridSearchCV(KNeighborsRegressor(), param)
+        # neigh = KNeighborsRegressor(n_neighbors=5, weights="distance")
+
+        y = rslt_data[column]
+        X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.33, random_state=42)
+        neigh.fit(X_train, y_train)
+        mse = mean_squared_error(y_test, neigh.predict(X_test))
+        print(mse)
+        print(neigh.best_params_, neigh.best_score_)
 
 
 if __name__ == '__main__':
@@ -55,12 +91,15 @@ if __name__ == '__main__':
     else:
         print(f'{diff_raw_processed} new samples to be processed')
 
-    data_to_be_processed = data_raw.loc[: diff_raw_processed - 1, :]
-    data_to_be_processed["region"] = data_to_be_processed["region"].apply(clean_region_sample)
-    data_to_be_processed["address"] = data_to_be_processed["address"].apply(clean_address_sample)
+    new_data = data_raw.loc[: diff_raw_processed - 1, :]
+    new_data["region"] = new_data["region"].apply(clean_region_sample)
+    new_data["address"] = new_data["address"].apply(clean_address_sample)
 
-    coordinates = data_to_be_processed["address"].apply(get_long_lat)
-    data_to_be_processed["latitude"], data_to_be_processed["longitude"] = zip(*coordinates)
+    coordinates = new_data["address"].apply(get_long_lat)
+    new_data["latitude"], new_data["longitude"] = zip(*coordinates)
 
-    data = pd.concat([data_to_be_processed, data_processed], ignore_index=True)
+    data = pd.concat([new_data, data_processed], ignore_index=True)
+    
+    # data = interpolate_missing_data_KNN(new_data, "price_increase")
+
     data.to_csv("hemnet_data/hemnet_house_data_processed.csv", index=False)
