c1ebc8c84235b9d709b417b27e07310e64eb1d8a
amandaabedahad <amanda.abedahad@outlook.com>
2022-09-14 11:08:41 +0200

    instead of mapping address to area using excel files, we now use shp file. each long/lat mapped to a polygon instead

--- map_address_to_area.py
+++ map_address_to_area.py
@@ -1,22 +1,27 @@
 import pandas as pd
 import re
+import geopandas as gpd
+from shapely.geometry import Point
 
+shp_file = "geospatial_data_polygons_areas/JUR_PRIMÄROMRÅDEN_XU_region.shp"
 hemnet_data = pd.read_csv("hemnet_data/hemnet_house_data_processed.csv")
-addresses_gothenburg = pd.read_excel("C:/Users/aabedaha/Downloads/Adressregister+Göteborg+september+2022.xlsx",
-                                     header=1)
-areas_gothenburg = pd.read_excel("C:/Users/aabedaha/Downloads/Områdeslista2022.xlsx")
-areas_gothenburg = areas_gothenburg.rename(columns={"Basområde (NYKO)": "Basområde"})
 
-for i, address in enumerate(hemnet_data["address"]):
-    if address.upper() in addresses_gothenburg["Adress"].apply(str.upper).values:
-        base_area = addresses_gothenburg[addresses_gothenburg["Adress"].apply(str.upper) == address.upper()]
-    else:
-        continue
+geo_data_raw = gpd.read_file(shp_file)
+geo_data = geo_data_raw[["PRIMÄROMRÅ", "PRIMÄRNAMN", "geometry"]]
+geo_data = geo_data.rename(columns={"PRIMÄRNAMN": "region"})
 
-    primary_area_row = areas_gothenburg[areas_gothenburg["Basområde"] == base_area["Basområde"].values[0]]
-    primary_area_raw = primary_area_row["Primärområde"].values[0]
-    primary_area = re.sub('\d+\s', "", primary_area_raw)
+# shp file has other lat/long reference system.
+# https://gis.stackexchange.com/questions/302699/extracting-longitude-and-latitude-from-shapefile
 
-    hemnet_data.loc[i, "region_NEW"] = primary_area
+geo_data["geometry"] = geo_data["geometry"].to_crs(epsg=4326)
 
+
+hemnet_data["coordinates"] = list(zip(hemnet_data["longitude"], hemnet_data["latitude"]))
+hemnet_data["coordinates"] = hemnet_data["coordinates"].apply(Point)
+
+points = gpd.GeoDataFrame(hemnet_data[["coordinates", "address", "latitude", "longitude"]], geometry="coordinates")
+points_to_region_map = gpd.tools.sjoin(points, geo_data, predicate="within")
+
+hemnet_data = hemnet_data.drop(["region_NEW", "coordinates", "region"], axis=1)
+hemnet_data = hemnet_data.merge(points_to_region_map[["region", "latitude", "longitude"]], on=["latitude", "longitude"])
 hemnet_data.to_csv("hemnet_data/hemnet_house_data_processed.csv", index=False)
