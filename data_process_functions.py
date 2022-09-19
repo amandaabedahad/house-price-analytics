da49b5284a34d747948713cd3ada2b10abbdc019
amandaabedahad <amanda.abedahad@outlook.com>
2022-09-19 12:04:47 +0200

    changed some variable names

--- data_process_functions.py
+++ data_process_functions.py
@@ -108,6 +108,7 @@ def interpolate_missing_data_KNN(dataframe, column):
 
 
 def map_address_to_area(hemnet_data, path_shp_file):
+    hemnet_data_copy = hemnet_data.copy()
     geo_data_raw = gpd.read_file(path_shp_file)
     geo_data = geo_data_raw[["PRIMÄROMRÅ", "PRIMÄRNAMN", "geometry"]]
     geo_data = geo_data.rename(columns={"PRIMÄRNAMN": "region"})
@@ -117,22 +118,19 @@ def map_address_to_area(hemnet_data, path_shp_file):
 
     geo_data["geometry"] = geo_data["geometry"].to_crs(epsg=4326)
 
-    hemnet_data["coordinates"] = list(zip(hemnet_data["longitude"], hemnet_data["latitude"]))
-    hemnet_data["coordinates"] = hemnet_data["coordinates"].apply(Point)
+    hemnet_data_copy["coordinates"] = list(zip(hemnet_data_copy["longitude"], hemnet_data_copy["latitude"]))
+    hemnet_data_copy["coordinates"] = hemnet_data_copy["coordinates"].apply(Point)
 
-    points = gpd.GeoDataFrame(hemnet_data[["coordinates", "address", "latitude", "longitude"]], geometry="coordinates")
+    points = gpd.GeoDataFrame(hemnet_data_copy[["coordinates", "address", "latitude", "longitude"]], geometry="coordinates")
     points.crs = "EPSG:4326"
     points_to_region_map = gpd.tools.sjoin(points, geo_data, predicate="within")
 
-    hemnet_data_nan_location = hemnet_data[hemnet_data["longitude"].isna()].drop("coordinates", axis=1)
-    rows_with_location_data = points_to_region_map.index
-    hemnet_data = hemnet_data.iloc[rows_with_location_data].drop(["coordinates", "region"], axis=1)
+    hemnet_data_nan_location = hemnet_data_copy[hemnet_data_copy["longitude"].isna()].drop("coordinates", axis=1)
+    hemnet_data_with_loc_data = hemnet_data_copy.loc[~hemnet_data_copy["longitude"].isna()].drop(["coordinates", "region"], axis=1)
 
-    hemnet_data = hemnet_data.merge(points_to_region_map[["region", "latitude", "longitude"]],
-                                    on=["latitude", "longitude"])
-    # we get some duplicates, ugly solution atm. should be solved before it happens
-    hemnet_data = hemnet_data[~hemnet_data.duplicated(keep="first")]
-    hemnet_data_mapped = pd.concat([hemnet_data, hemnet_data_nan_location])
+    hemnet_data_merged = hemnet_data_with_loc_data.merge(points_to_region_map[["region", "latitude", "longitude"]],
+                                    on=["latitude", "longitude"]).drop_duplicates()
+    hemnet_data_mapped = pd.concat([hemnet_data_merged, hemnet_data_nan_location])
     # hemnet_data.to_csv("hemnet_data/hemnet_house_data_processed.csv", index=False)
     return hemnet_data_mapped
 
