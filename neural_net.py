820c550a52282e8161cd6e8e96e24d232bf755e6
amandaabedahad <amanda.abedahad@outlook.com>
2022-09-26 11:18:33 +0200

    simple mlp, model is saved and then used by app.py

--- neural_net.py
+++ neural_net.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -8,6 +10,7 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import seaborn as sns
+from sklearn.model_selection import KFold
 
 
 class Data(Dataset):
@@ -24,7 +27,7 @@ class Data(Dataset):
 
 
 class Simple_nn(nn.Module):
-    def __init__(self, input_features_dim, output_dim, hidden_dim):
+    def __init__(self, input_features_dim=4, output_dim=1, hidden_dim=32):
         super(Simple_nn, self).__init__()
         self.fc1 = nn.Linear(input_features_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
@@ -41,11 +44,21 @@ class Simple_nn(nn.Module):
         return x
 
 
+def get_rolling_average_data(data, window):
+    average_data = []
+    for ind in range(len(data) - window + 1):
+        average_data.append(np.mean(data[ind:ind + window]))
+    return average_data
+
+
 if __name__ == "__main__":
     batch_size = 16
-    nr_epochs = 500
+    nr_epochs = 200
+    k_folds = 5
     loss_values = []
     percentage_off_values = []
+
+    k_fold = KFold(n_splits=k_folds, shuffle=True)
 
     hemnet_house_data = pd.read_csv("hemnet_data/hemnet_house_data_processed.csv")
     filtered_data = hemnet_house_data[hemnet_house_data["housing_type"] == "Lägenhet"]
@@ -54,7 +67,7 @@ if __name__ == "__main__":
     X = data.drop(columns=["final_price"])
     std_X = StandardScaler()
     X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.33, random_state=42)
-    sns.pairplot(X)
+    # sns.pairplot(X)
     X_train = std_X.fit_transform(X_train)
     X_test = std_X.transform(X_test)
 
@@ -63,10 +76,10 @@ if __name__ == "__main__":
 
     test_data = Data(X_test, y_test)
     test_dataloader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True)
-
-    criterion = nn.MSELoss()
+    # TODO: implement cross validation - good for small datasets
+    criterion = nn.HuberLoss()
     net = Simple_nn(input_features_dim=4, output_dim=1, hidden_dim=32)
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
     for epoch in range(nr_epochs):
         for X, y in train_dataloader:
@@ -74,17 +87,22 @@ if __name__ == "__main__":
             pred = net(X)
             loss = criterion(pred, torch.reshape(y, (-1, 1)))
             loss_values.append(loss.item())
-            percentage_off = y/pred
-            percentage_off_values.append(percentage_off.mean().item())
+            y_formatted = torch.reshape(y, (-1, 1))
+            percentage_off = torch.abs(y_formatted - pred) / y_formatted * 100
+            percentage_off_values.append(torch.mean(percentage_off).item())
             loss.backward()
             optimizer.step()
 
-    step = np.linspace(0, nr_epochs, 91000)
-
+    step = np.linspace(0, nr_epochs, len(percentage_off_values))
+    rolling_average = get_rolling_average_data(percentage_off_values, 100)
     fig, ax = plt.subplots(figsize=(8, 5))
     plt.plot(step, np.array(percentage_off_values))
-    plt.title("Step-wise Loss")
+    plt.plot(np.linspace(0, nr_epochs, len(rolling_average)), rolling_average, label="rolling average")
+    plt.title(f"Percentage off, prediction and true. Mean percentage off {rolling_average[-1]}")
+    plt.legend()
     plt.xlabel("Epochs")
-    plt.ylabel("Loss")
+    plt.ylabel("Prediction off [%]")
     plt.show()
+    pickle.dump(std_X, open("standard_scaler.pkl", "wb"))
+    torch.save(net.state_dict(), "nn_model.pkl")
 
